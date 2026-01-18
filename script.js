317927e2ead5365a40244acaf7344ec0b973501a
Shamee15 <leehameed5@gmail.com>
2026-01-18 20:46:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Highlight active menu
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    links.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
+
